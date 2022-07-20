6c1e2f9471c0f13233f1c3eca63e74eb4b0e9dd2
hilasent <hilasentt@gmail.com>
2022-07-20 16:26:31 +0300

    trying bootstrap utilies

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  
+
 
   constructor() { }
 
   title = "Ürün Listesi";
 
-  products: any =[
-     {id:1, name:"laptop", price:2500, categoryId:1, description:"asdfgh"},
-     {id:2, name:"mouse", price:25, categoryId:2, description:"zxcvb"}
+  products: any = [
+    { id: 1, name: "laptop", price: 2500, categoryId: 1, description: "asdfgh" },
+    { id: 2, name: "mouse", price: 25, categoryId: 2, description: "zxcvb" }
   ]
 
   ngOnInit(): void {
